1c468d3717bffa7a46e3510a822bbf8a413074ac
kalamnsi10 <123523157+kalamansi10@users.noreply.github.com>
2023-11-23 05:44:08 +0800

    Cart controller initial configuration

--- app/controllers/api/v1/cart_controller.rb
+++ app/controllers/api/v1/cart_controller.rb
@@ -1,4 +1,23 @@
 class Api::V1::CartController < ApplicationController
+  def get_cart
+    render json: current_user.carted.order(created_at: :desc), include: {seed: {only: [:name, :price, :image_links]}}
+  end
   def add_to_cart
+    cart_item = current_user.carted.find_by(item_id: params[:item_id])
+
+    if cart_item
+      cart_item.update(amount: params[:amount].to_i + cart_item.amount)
+    else
+      carted = current_user.carted.build
+      carted.item_id = params[:item_id]
+      carted.amount = params[:amount]
+      carted.save
+    end
+  end
+  def update_cart
+    Carted.find(params[:carted_id]).update(amount: params[:amount])
+  end
+  def remove_from_cart
+    Carted.find(params[:carted_id]).destroy
   end
 end
