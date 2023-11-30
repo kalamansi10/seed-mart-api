85f0c4147bc07530ad9ab687cd3e014eb253f902
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-12-01 05:14:27 +0800

    Added extra params to user registration and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: "users/sessions",
+    registrations: "users/registrations" 
   }
 
   concern :shop_queries do
