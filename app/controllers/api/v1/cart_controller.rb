77d6937b36a9d8a27986b27a1bb29bebe6aa548d
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-12-05 16:07:11 +0800

    Removed checkout indicator on carted items model

--- app/controllers/api/v1/cart_controller.rb
+++ app/controllers/api/v1/cart_controller.rb
@@ -1,10 +1,9 @@
 class Api::V1::CartController < ApplicationController
   def get_cart
-    render json: current_user.carted.order(created_at: :desc), include: {seed: {only: [:name, :price, :image_links]}}
+    render json: current_user.carted.order(created_at: :desc), include: :seed
   end
   def add_to_cart
     cart_item = current_user.carted.find_by(item_id: params[:item_id])
-
     if cart_item
       cart_item.update(amount: params[:amount].to_i + cart_item.amount)
     else
@@ -14,7 +13,6 @@ class Api::V1::CartController < ApplicationController
   def update_cart
     carted = Carted.find(params[:carted_id])
     carted.update(amount: params[:amount]) if params[:amount]
-    carted.update(checkout: params[:checkout]) if params[:checkout]
   end
   def remove_from_cart
     Carted.find(params[:carted_id]).destroy
