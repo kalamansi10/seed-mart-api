bafc0b96b9acdf480a22c0bde9ad9d0d3102fdad
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-12-05 15:53:07 +0800

    Updated registrarion params

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,16 +42,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:fname, :lname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, 
-                                       keys: [:fname, 
-                                              :lname, 
-                                              :gender, 
-                                              :birthday, 
+    devise_parameter_sanitizer.permit(:account_update,
+                                       keys: [:name,
+                                              :gender,
+                                              :birthday,
                                               :adresses,
                                               :payment_method
                                             ])
