0460049bfd435183b5ee287120ca8146029752ff
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-12-08 11:18:25 +0800

    Methods for adding and removing shipping address

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -2,4 +2,16 @@ class Api::V1::UserController < ApplicationController
   def get_shipping_addresses
     render json: current_user.shipping_addresses
   end
+  def add_shipping_addresses
+    current_user.shipping_addresses.create(shipping_address_params)
+  end
+  def update_shipping_address
+  end
+  def remove_shipping_address
+    ShippingAddress.find(params[:shipping_address_id]).destroy
+  end
+  private
+  def shipping_address_params
+    params.require(:shipping_address).permit(:street_address, :barangay, :city, :province, :region)
+  end
 end
