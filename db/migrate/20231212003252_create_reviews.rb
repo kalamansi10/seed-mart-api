e1b321d0ead22cab6ec670572066454168f2f671
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-12-12 12:26:06 +0800

    Added Review model

---
+++ db/migrate/20231212003252_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[7.1]
+  def change
+    create_table :reviews do |t|
+      t.bigint :user_id, null: false
+      t.bigint :item_id, null: false
+      t.integer :rating, null: false
+      t.text :comment
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_06_011839) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_12_003252) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,7 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_06_011839) do
     t.string "planting_method"
     t.string "package_size"
     t.integer "price"
-    t.integer "items_sold"
     t.text "tags"
     t.text "image_links", default: ["https://placehold.co/600x400", "https://placehold.co/400x600", "https://placehold.co/400x400", "https://placehold.co/600x600"], array: true
     t.datetime "created_at", null: false
@@ -68,6 +67,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_06_011839) do
     t.string "payment_type"
     t.string "card_number"
     t.string "expiry_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.integer "rating", null: false
+    t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
