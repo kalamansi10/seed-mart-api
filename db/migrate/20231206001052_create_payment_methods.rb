ff2fb3425eea211726f03c897a312b1071c78ed1
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-12-19 23:24:02 +0800

    Added is_main column

--- db/migrate/20231206001052_create_payment_methods.rb
+++ db/migrate/20231206001052_create_payment_methods.rb
@@ -1,9 +1,11 @@
 class CreatePaymentMethods < ActiveRecord::Migration[7.1]
   def change
     create_table :payment_methods do |t|
-      t.string :payment_type
-      t.string :card_number
-      t.string :expiry_date
+      t.string :payment_type, null: false
+      t.string :card_number, null: false
+      t.string :expiry_date, null: false
+      t.boolean :is_main, default: false, null: false
+
       t.timestamps
     end
   end
