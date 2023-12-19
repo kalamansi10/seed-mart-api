c382717afb5daf736d30c008ce11f40e678a8fb3
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-12-19 23:23:48 +0800

    Added setting up main address when adding new

--- db/schema.rb
+++ db/schema.rb
@@ -64,9 +64,10 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_12_003252) do
   end
 
   create_table "payment_methods", force: :cascade do |t|
-    t.string "payment_type"
-    t.string "card_number"
-    t.string "expiry_date"
+    t.string "payment_type", null: false
+    t.string "card_number", null: false
+    t.string "expiry_date", null: false
+    t.boolean "is_main", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
