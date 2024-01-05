374d8c20f503af7ced6b862a7bc7bd0aca81f668
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-01-05 09:51:59 +0800

    Renamed methods and added is_cart_empty?

--- app/controllers/api/v1/cart_controller.rb
+++ app/controllers/api/v1/cart_controller.rb
@@ -2,9 +2,11 @@ class Api::V1::CartController < ApplicationController
   def get_cart
     render json: current_user.carted_items.order(created_at: :desc), include: :item
   end
+
   def get_for_checkout
     render json: current_user.carted_items.where(is_for_checkout: true).order(created_at: :desc), include: :item
   end
+
   def add_to_cart
     carted_item = current_user.carted_items.find_by(item_id: params[:item_id])
     if carted_item
@@ -13,12 +15,24 @@ class Api::V1::CartController < ApplicationController
       current_user.carted_items.create({item_id:params[:item_id], amount:params[:amount]})
     end
   end
-  def update_cart
-    carted_item = CartedItem.find(params[:carted_id])
-    carted_item.update(amount: params[:amount]) if params[:amount]
-    carted_item.update(is_for_checkout: params[:is_for_checkout]) if params[:is_for_checkout]
+
+  def update_carted_amount
+    return unless params[:amount] && params[:carted_id]
+    CartedItem.find(params[:carted_id])
+              .update(amount: params[:amount])
   end
+
+  def update_checkout_status
+    return unless params[:is_for_checkout] && params[:carted_id]
+    CartedItem.find(params[:carted_id])
+              .update(is_for_checkout: params[:is_for_checkout])
+  end
+
   def remove_from_cart
     CartedItem.find(params[:carted_id]).destroy
+  end
+
+  def is_cart_empty?
+    render json: { is_cart_empty: current_user.carted_items.empty? }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,19 +15,21 @@ Rails.application.routes.draw do
     get "/get-cart", to: "cart#get_cart"
     get "/get-for-checkout", to: "cart#get_for_checkout"
     post "/add-to-cart", to: "cart#add_to_cart"
-    post "/update-cart", to: "cart#update_cart"
+    put "/update-carted-amount/:carted_id/:amount", to: "cart#update_carted_amount"
+    put "/update-checkout-status/:carted_id/:is_for_checkout", to: "cart#update_checkout_status"
     delete "/remove-from-cart/:carted_id", to: "cart#remove_from_cart"
+    get "/is-cart-empty", to: "cart#is_cart_empty?"
   end
 
   concern :user_actions do
     get "/get-shipping-addresses", to: "user#get_shipping_addresses"
     post "/add-shipping-address", to: "user#add_shipping_addresses"
-    post "/update-shipping-address", to: "user#update_shipping_address"
+    put "/update-shipping-address", to: "user#update_shipping_address"
     delete "/remove-shipping-address/:shipping_address_id", to: "user#remove_shipping_address"
 
     get "/get-payment-methods", to: "user#get_payment_methods"
     post "/add-payment-method", to: "user#add_payment_method"
-    post "/update-payment-method", to: "user#update_payment_method"
+    put "/update-payment-method", to: "user#update_payment_method"
     delete "/remove-payment-method/:payment_method_id", to: "user#remove_payment_method"
 
     get "/get-orders", to: "user#get_orders"
