e898ca5747a1ba3a45b9e1cda5b3285782f0a0ea
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-01-07 14:15:06 +0800

    Added destroy dependency

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
   has_many :carted_items
-  has_many :orders
-  has_many :reviews
+  has_many :orders, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 end
