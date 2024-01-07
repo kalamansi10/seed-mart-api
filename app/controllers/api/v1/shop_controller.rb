2bed9876ac95866bddffd16d5d009c30b977cb5f
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-01-07 14:18:37 +0800

    Added item mapper with number sold

--- app/controllers/api/v1/shop_controller.rb
+++ app/controllers/api/v1/shop_controller.rb
@@ -1,13 +1,17 @@
 class Api::V1::ShopController < ApplicationController
-
   # GET /api/v1/search
   def search
     keyword = params[:keyword].downcase
     offset = params[:offset]
+
     # Filter items based on keyword and additional filters
-    item_list = item_sorter(item_filter(price_filter(Item))).where("tags LIKE ?", "%#{keyword}%")
+    item_list = item_sorter(item_filter(price_filter(Item)))
+                .where("tags LIKE ?", "%#{keyword}%")
+                .limit(20)
+                .map { |item| item_details(item) }
+
     render json: {
-      item_list: item_list.offset(offset.presence).limit(20),
+      item_list: item_list,
       item_count: item_list.count
     }
   end
@@ -26,24 +30,23 @@ class Api::V1::ShopController < ApplicationController
 
   # GET /api/v1/items_properties
   def items_properties
-    filters = Item.column_names
+    filters = Item.column_names - ["id", "name", "price", "items_sold", "tags", "image_links", "created_at", "updated_at"]
     # Retrieve distinct property values for items
-    render json: filters.map { |attribute| [attribute, Item.distinct.pluck(attribute)] }
-                      .to_h.except("id", "name", "price", "items_sold", "tags", "image_links", "created_at", "updated_at")
+    render json: filters.map { |attribute| [attribute, Item.distinct.pluck(attribute)] }.to_h
   end
 
   private
 
   # Sorts item list
   def item_sorter(item_list)
-  case params[:sort_by]
-  when nil
-    return item_list.order(created_at: :desc)
-  when "price-lowest"
-    return item_list.order(price: :asc)
-  when "price-highest"
-    return item_list.order(price: :desc)
-  end
+    case params[:sort_by]
+    when nil
+      return item_list.order(created_at: :desc)
+    when "price-lowest"
+      return item_list.order(price: :asc)
+    when "price-highest"
+      return item_list.order(price: :desc)
+    end
   end
 
   # Apply additional filters to the item list
@@ -62,5 +65,16 @@ class Api::V1::ShopController < ApplicationController
     item_list = item_list.where("price >= ?", params[:minimum]) unless params[:minimum].blank?
     item_list = item_list.where("price <= ?", params[:maximum]) unless params[:maximum].blank?
     item_list
+  end
+
+  # Extract details for each item
+  def item_details(item)
+    {
+      id: item.id,
+      name: item.name,
+      price: item.price,
+      preview_image: item.image_links[0],
+      items_sold: item.orders.where(status: 'received').count
+    }
   end
 end
