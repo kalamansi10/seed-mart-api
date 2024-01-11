788d0cbaea1509523af7b4c9ea1f830cbad4d133
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-01-11 14:23:53 +0800

    Added model method item_sold and average_rating

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,28 @@ class Item < ApplicationRecord
   has_many :carted_items
   has_many :orders, dependent: :destroy
   has_many :reviews, dependent: :destroy
+
+  def self.get(item_id)
+    includes(:reviews, :orders)
+      .find(item_id)
+      .as_json(
+        include: {
+          reviews: {},
+        },
+        methods: [:average_rating, :item_sold],
+        except: [:created_at, :updated_at]
+      )
+  end
+
+  def item_sold
+    orders.where(status: 'completed').count
+  end
+
+  def average_rating
+    total_reviews = reviews.count
+    return 0 if total_reviews.zero?
+
+    total_rating = reviews.pluck(:rating).reduce(:+)
+    total_rating.to_f / total_reviews.round
+  end
 end
