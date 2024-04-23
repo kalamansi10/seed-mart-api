594675ce25eb762bd16a1bbe5774f07e3135004c
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-23 21:36:46 +0800

    Refactored methods and added get_review action

--- app/controllers/api/v1/review_controller.rb
+++ app/controllers/api/v1/review_controller.rb
@@ -1,31 +1,39 @@
 class Api::V1::ReviewController < ApplicationController
     # GET /api/v1/review/list/:item_id
-    def getReviewList
+    def get_review_list
       reviews = Item.find(params[:item_id]).reviews.order(created_at: :desc)
-      reviews = addReviewer(reviews)
+      reviews = add_reviewer(reviews)
       render json: reviews
     end
 
+    # GET /api/v1/review/:order_id
+    def get_review
+      review = Review.find_by(order_id: params[:order_id])
+      render json: review
+    end
+
     # POST /api/v1/review
-    def addReview
+    def add_review
       current_user.reviews.create(review_params)
     end
     # PUT /api/v1/review
-    def editReview
-      current_user.reviews.update(review_params)
+    def edit_review
+      review = Review.find(params[:review_id]).update(review_params)
     end
     # DELETE /api/v1/review
-    def deleteReview
+    def delete_review
       Review.find(params[:review_id]).destroy
     end
 
     private
 
-    def addReviewer(reviews)
+    def add_reviewer(reviews)
       reviews.map do |review|
-        review_hash = review.attributes
-        return review_hash if review.is_anonymous?
-        review_hash.merge({ reviewer: review.user.name })
+        if review.is_anonymous
+          { **review.attributes, reviewer: "Anonymous" }
+        else
+          { **review.attributes, reviewer: review.user.name }
+        end
       end
     end
 
@@ -34,7 +42,8 @@ class Api::V1::ReviewController < ApplicationController
         :item_id,
         :order_id,
         :rating,
-        :comment
+        :comment,
+        :is_anonymous
       )
     end
 
