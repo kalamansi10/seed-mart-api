48259205ff928d75aeb434f1db711b30ada9a7b0
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-27 13:51:05 +0800

    Set same site cookies config to lax

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,9 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # Specify cookies SameSite protection level: either :none, :lax, or :strict.
+  #
+  # This change is not backwards compatible with earlier Rails versions.
+  # It's best enabled when your entire app is migrated and stable on 6.1.
+  config.action_dispatch.cookies_same_site_protection = :lax
 end
